60326d5c18c8eb04f5576552e780ec159ceaaa9d
Michał Mika <michal@mika.sh>
2024-03-20 16:21:44 +0100

    Initialized new registry.

---
+++ Registry.lua
@@ -0,0 +1,9 @@
+local Registry = {
+    name = "CosmRegistry",
+    uuid = "85c8089d-1f17-44f2-bdd4-7cc9284552d9",
+    url  = "git@github.com:m1ka05/CosmRegistry.git",
+    description = "Cosm local package registry",
+    packages = {
+    },
+}
+return Registry
